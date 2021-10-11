881de9b49e399e263d1d9368b47386ceb21ce1fd
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-11 15:56:28 +0200

    inserimento domande per l'utente

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Chiedere all'utente quanti km vuole percorrere
-
+const userRandomOne = prompt(" Quanti km vuoi percorrere? ");
 // Chiedere all'utente l'età
+
+const userRandomTwo = prompt(" Quanti anni hai?");
+
 
